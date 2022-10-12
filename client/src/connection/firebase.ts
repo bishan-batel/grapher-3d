f18ac876bc53787df8abb4c1b623d312ebf065d2
bishan <bishan.batel@gmail.com>
2022-10-11 22:10:01 -0500

    Added firebase

---
+++ client/src/connection/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+  apiKey: "AIzaSyBizO5E9t1Injj5jO77a2slpsQPdbl-JRg",
+  authDomain: "grapher-3d.firebaseapp.com",
+  projectId: "grapher-3d",
+  storageBucket: "grapher-3d.appspot.com",
+  messagingSenderId: "417831012247",
+  appId: "1:417831012247:web:8d0bf633212c48a82b66d2",
+});
+
+export default app;
